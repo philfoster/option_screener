a2adf42e9f39047c9d12d61c7a4827e668bf4fb6
Philip Foster <pfoster@Philips-MacBook-Pro.local>
2017-09-14 02:59:45 -0400

    Adding CSV output

--- src/screener.py
+++ src/screener.py
@@ -9,8 +9,23 @@ max_ask = 10
 max_days = 45
 min_days = 21
 max_attempts = 3
+max_pe_ratio = 30
+min_yield = 1
+max_price = 100
+
+break_even_percent = 2
+min_break_even_factor = 1 + ( break_even_percent / 100.0 )
+
+min_target_price_percent = 20
+min_target_price_factor = 1 + ( min_target_price_percent / 100.0 )
+
+target_price_percent = 5
+target_price_factor = 1 + ( target_price_percent / 100.0 )
+commission_cost = 7.73
 
 base_url = "http://www.marketwatch.com/investing/stock/"
+
+output_csv = "./call_options.csv"
 
 def get_symbols ( filename ):
 	symbols = list()
@@ -31,6 +46,7 @@ def get_details ( symbol ):
 	details["symbol"] = symbol
 	details["Yield"] = "None"
 	details["Ex-Dividend Date"] = "None"
+	details["P/E Ratio"] = 99999
 	details_url = "{0}{1}".format ( base_url, symbol )
 	page_data = fetch_url ( details_url )
 
@@ -69,7 +85,7 @@ def get_details ( symbol ):
 		
 	return details
 
-def get_option_calls ( symbol ):
+def get_option_calls ( symbol, price ):
 	option_data = dict()
 	options_url = "https://finance.yahoo.com/quote/{0}/options?p={0}".format ( symbol )
 	now = time.time()
@@ -85,11 +101,13 @@ def get_option_calls ( symbol ):
 			print "Date({0}): {1} is too far in the future".format( symbol, date )
 			continue
 
-		data = get_option_chain_by_date ( symbol, date )
-		option_data[date] = data
+		data = get_option_chain_by_date ( symbol, date, price )
+		date_string = time.strftime('%Y-%m-%d',  time.gmtime(date))
+
+		option_data[date_string] = data
 	return option_data
 
-def get_option_chain_by_date ( symbol, date ):
+def get_option_chain_by_date ( symbol, date, price ):
 	calls = dict()
 	url = "https://finance.yahoo.com/quote/{0}/options?p={0}&date={1}".format ( symbol, date )
 
@@ -98,13 +116,13 @@ def get_option_chain_by_date ( symbol, date ):
 		match = re.search ( 'quote/(\S+)\?p=', call )
 		if match:
 			call_id = match.group(1)
-			call_data = get_call_data ( symbol, call_id )
+			call_data = get_call_data ( symbol, call_id, price )
 			if call_data:
 				calls[call_id] = call_data
 
 	return calls
 
-def get_call_data ( symbol, call_id ):
+def get_call_data ( symbol, call_id, price ):
 	call_data = dict()
 	call_data["ask"] = "unknown"
 	call_data["bid"] = "unknown"
@@ -117,15 +135,15 @@ def get_call_data ( symbol, call_id ):
 
 	ask_match = re.search ( '"ask":{"raw":([\d\.]+),', page_data )
 	if ask_match:
-		call_data["ask"] = ask_match.group(1)
+		call_data["ask"] = float(ask_match.group(1))
 
 	bid_match = re.search ( '"bid":{"raw":([\d\.]+),', page_data )
 	if bid_match:
-		call_data["bid"] = bid_match.group(1)
+		call_data["bid"] = float(bid_match.group(1))
 
 	strike_match = re.search ( '"strikePrice":{"raw":([\d\.]+),', page_data )
 	if strike_match:
-		call_data["strikePrice"] = strike_match.group(1)
+		call_data["strikePrice"] = float(strike_match.group(1))
 
 	int_match = re.search ( '"openInterest":{"raw":([\d\.]+),', page_data )
 	if int_match:
@@ -142,6 +160,27 @@ def get_call_data ( symbol, call_id ):
 	if call_data["openInterest"] < min_open_interest:
 		print "Skipping call id '{0}', open interest is too low ({1})".format ( call_id, call_data["openInterest"] )
 		return None
+
+	# Check to see if it's a viable call
+	# It should not take more than 2% climb in price to break even
+	break_even = float(call_data["strikePrice"]) + float(call_data["ask"])
+
+	min_viable_move = price * min_break_even_factor
+	print "Examining {0} call ask {1} - break even at {2}".format ( call_data["strikePrice"], call_data["ask"], break_even )
+	if  break_even > min_viable_move:
+		print "Skipping call id '{0}', break even point ({1}) is higher than price ({2}) plus {3}% ({4})".format ( call_id, break_even, price, break_even_percent, min_viable_move)
+		return None
+
+	# Check to see if a 5% upswing yields a big enough gain
+	cost_to_buy = 100 * call_data["ask"] + commission_cost
+	target_price = price * target_price_factor
+	gain_at_target_price = target_price - price
+	proceeds = ( gain_at_target_price * 100 ) - commission_cost
+
+	if proceeds < ( cost_to_buy * min_target_price_factor ):
+		print "Skipping call id '{0}', proceeds ({1}) are less than minimum target price factor ({2})".format ( call_id, proceeds, cost_to_buy * min_target_price_factor )
+		return None
+
 	return call_data
 
 def get_option_dates ( url ):
@@ -163,7 +202,7 @@ def fetch_url ( url ):
 		try:
 			page_data = urlread ( url )
 			break
-		except Error as e:
+		except Exception as e:
 			print "Error: {0}".format(e)
 			attempts = attempts + 1
 			time.sleep ( 5 )
@@ -172,11 +211,69 @@ def fetch_url ( url ):
 
 symbols = get_symbols ( "symbols.txt" )
 
+viable_calls = set()
+
 for symbol in symbols:
 	# Details data
 	data = get_details ( symbol )
 
+	# Check the price
+	if float(data["price"]) > max_price:
+		print "Skipping {0}, price is too high {1}".format ( symbol, data["price"] )
+		continue
+
+	# Check the p/e ratio
+	if float(data["P/E Ratio"]) > max_pe_ratio:
+		print "Skipping {0}, P/E Ration is too high {1}".format ( symbol, data["P/E Ratio"] )
+		continue
+
+	# Check the yield
+	yield_match = re.search ( '^([\d\.]+)\%?', data["Yield"] )
+	if yield_match:
+		div_yield = yield_match.group(1)
+		if float(div_yield) < min_yield:
+			print "Skipping {0}, yield is too low {1}".format ( symbol, data["Yield"] )
+			continue
+	else:
+		print "Skipping {0}, yield is too low {1}".format ( symbol, data["Yield"] )
+		continue
+
 	# Option chain
-	data["option_calls"] = get_option_calls ( symbol )
+	data["call_options"] = get_option_calls ( symbol, float(data["price"]) )
 	print data
 	
+	print "--------------\n"
+	print "Symbol: {0}".format ( symbol )
+	print "Price: {0}".format ( data["price"] )
+	print "P/E Ratio: {0}".format ( data["P/E Ratio"] )
+	print "Yield: {0}".format ( data["Yield"] )
+	print "Ex-Dividend Date: {0}".format ( data["Ex-Dividend Date"] )
+	for date_key in data["call_options"]:
+		for call_id in data["call_options"][date_key]:
+			bid = data["call_options"][date_key][call_id]["bid"]
+			ask = data["call_options"][date_key][call_id]["ask"]
+			strike = data["call_options"][date_key][call_id]["strikePrice"]
+			print "Viable option: {0} {1} call ask {2}".format( date_key, strike, ask )
+
+			good_call = (symbol,data["price"],data["P/E Ratio"],data["Yield"],data["Ex-Dividend Date"],date_key,strike,ask)
+			viable_calls.add ( good_call )
+
+f = file ( output_csv, "w" )
+f.write ( "Symbol,Price,P/E Ratio,Yield,Ex-Dividend Date,Expiration Date,Strike Price,Ask,Break Even,Cost,Price at +{0}%,Proceeds at +{0}%,Gain$ at +{0}%, Gain% at +{0}%\n".format ( target_price_percent ) )
+
+print "Writing results to {0}".format(output_csv)
+for call in viable_calls:
+	(symbol,price,pe_ratio,div_yield,div_date,exp_date,strike,ask) = call
+
+	price_at_target = float(price) * target_price_factor
+	gain_at_target = price_at_target - strike
+	proceeds_at_target = ( gain_at_target * 100 ) - commission_cost
+
+	cost_to_buy = ( ask * 100 ) + commission_cost
+
+	profit_dollars = proceeds_at_target - cost_to_buy
+	profit_percent = ( ( profit_dollars / cost_to_buy ) - 1 ) * 100
+
+	f.write("{0},{1},{2},{3},\"{4}\",{5},{6},{7},{8},{9},{10},{11},{12},{13:.2f}%\n".format ( symbol, price, pe_ratio, div_yield,div_date,exp_date,strike,ask,(strike + ask),cost_to_buy,price_at_target,proceeds_at_target,profit_dollars,profit_percent) )
+
+f.close()
