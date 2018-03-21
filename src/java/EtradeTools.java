8f224d78bb8078346b3b7ab466343e15b5fb8159
Phil Foster <phil@the-fosters.net>
2018-03-21 11:07:29 -0400

    Added batching of quotes

--- src/java/EtradeTools.java
+++ src/java/EtradeTools.java
@@ -40,6 +40,8 @@ class EtradeTools {
     public static int LIVE = 1;
     public static int SANDBOX = 0;
 
+    public static int MAX_BATCH_SIZE = 1; // TODO - set this to 20 when using live key
+
     public static AuthToken getAuthToken ( String key, String secret, int env ) throws IOException, ETWSException {
 
         IOAuthClient authClient = OAuthClientImpl.getInstance();
@@ -125,30 +127,62 @@ class EtradeTools {
     }
 
     public static List<QuoteData> getQuote ( AuthToken authToken, String symbol ) { 
-        ClientRequest clientRequest = getAccessRequest ( authToken );
-        
         ArrayList<String> symbols = new ArrayList<String>();
+        symbols.add(symbol);
+        return getQuote ( authToken, symbols );
+    }
+
+    public static List<QuoteData> getQuote ( AuthToken authToken, ArrayList<String> symbols ) { 
+        ClientRequest clientRequest = getAccessRequest ( authToken );
         MarketClient marketClient = new MarketClient(clientRequest);
 
-
-        System.out.println ( "Fetching quote for " + symbol );
-        symbols.add(symbol);
-        
         QuoteResponse quoteResponse = new QuoteResponse();
+        ArrayList<QuoteData> allResponses = new ArrayList<QuoteData>();
 
-        try {
-            quoteResponse = marketClient.getQuote(symbols, new Boolean(true), DetailFlag.ALL);
-        } catch (IOException ex) {
-            System.out.println ( "caught exception: " + ex );
-            ex.printStackTrace();
-            System.exit(1);
-        } catch (ETWSException ex) {
-            System.out.println ( "caught exception: " + ex );
-            ex.printStackTrace();
-            System.exit(1);
+        int count = 0;
+        int total = 0;
+
+        System.out.println ( "Got " + count + " symbols" );
+
+        ArrayList<String> batch = new ArrayList<String>();
+        while ( count < symbols.size() ) {
+
+            batch.add ( symbols.get ( count ) );
+            count++;
+
+            if ( count % MAX_BATCH_SIZE == 0 || count == symbols.size() ) {
+                try {
+                    System.out.println ( "Fetching batch of size " + batch.size() );
+                    total += batch.size();
+                    quoteResponse = marketClient.getQuote( batch, new Boolean(true), DetailFlag.ALL);
+                } catch (IOException ex) {
+                    System.out.println ( "caught exception: " + ex );
+                    ex.printStackTrace();
+                    System.exit(1);
+                } catch (ETWSException ex) {
+                    System.out.println ( "caught exception: " + ex );
+                    ex.printStackTrace();
+                    System.exit(1);
+                }
+
+                // Now clear out the batch for the next run
+                batch.clear();
+
+                System.out.println ( "Number of quotes in the response: " + quoteResponse.getQuoteData().size() );
+                for ( QuoteData q : quoteResponse.getQuoteData() ) {
+                    System.out.println ( "Adding response for quote: " + q );
+                    allResponses.add ( q );
+                }
+            }
         }
 
-        return quoteResponse.getQuoteData();
+        if ( total == symbols.size() ) {
+            System.out.println ( "\n\n\nPerfect! we matched the right number\n\n\n" );
+        } else {
+            System.out.println ( "\n\n\nShit! we wanted " + symbols.size() + " but only got " + total );
+        }
+
+        return allResponses;
     }
     
 
@@ -334,42 +368,40 @@ class EtradeTools {
 
         Pattern regexPattern = Pattern.compile("\\$(\\d\\S*) (Call|Put)");
 
-        for ( String quoteSymbol : symbolBatch ) {
-            for ( QuoteData quoteData : getQuote ( authToken, quoteSymbol ) ) {
+        for ( QuoteData quoteData : getQuote ( authToken, symbolBatch ) ) {
 
-                String symbolDesc = quoteData.getAll().getSymbolDesc();
-                String underlier = quoteData.getProduct().getSymbol();
-                Double strike = new Double ( 0.0 );
+            String symbolDesc = quoteData.getAll().getSymbolDesc();
+            String underlier = quoteData.getProduct().getSymbol();
+            Double strike = new Double ( 0.0 );
 
-                String type;
+            String type;
 
-                // GOOG Apr 16 '11 $350 Put
-                Matcher match = regexPattern.matcher(symbolDesc);
-                if ( match.find() ) {
-                    strike = new Double ( match.group(1) );
-                    type = match.group(2);
-                } else {
-                    continue;
-                }
-
-                OptionChainQuote quote;
-                if ( type.equals ( "Call" ) ) {
-                    quote = new CallOptionQuote ( underlier + "(" + symbolDesc + ")", date, strike.doubleValue() );
-                } else {
-                    quote = new PutOptionQuote ( underlier + "(" + symbolDesc + ")", date, strike.doubleValue() );
-                }
-
-                quote.setBid ( quoteData.getAll().getBid() );
-                quote.setAsk ( quoteData.getAll().getAsk() );
-
-                quote.setBidSize ( (int) quoteData.getAll().getBidSize() );
-                quote.setAskSize ( (int) quoteData.getAll().getAskSize() );
-
-                quote.setLastTrade ( quoteData.getAll().getLastTrade() );
-                quote.setOpenInterest ( (int) quoteData.getAll().getOpenInterest() );
-
-                chain.add ( quote );
+            // GOOG Apr 16 '11 $350 Put
+            Matcher match = regexPattern.matcher(symbolDesc);
+            if ( match.find() ) {
+                strike = new Double ( match.group(1) );
+                type = match.group(2);
+            } else {
+                continue;
             }
+
+            OptionChainQuote quote;
+            if ( type.equals ( "Call" ) ) {
+                quote = new CallOptionQuote ( underlier + "(" + symbolDesc + ")", date, strike.doubleValue() );
+            } else {
+                quote = new PutOptionQuote ( underlier + "(" + symbolDesc + ")", date, strike.doubleValue() );
+            }
+
+            quote.setBid ( quoteData.getAll().getBid() );
+            quote.setAsk ( quoteData.getAll().getAsk() );
+
+            quote.setBidSize ( (int) quoteData.getAll().getBidSize() );
+            quote.setAskSize ( (int) quoteData.getAll().getAskSize() );
+
+            quote.setLastTrade ( quoteData.getAll().getLastTrade() );
+            quote.setOpenInterest ( (int) quoteData.getAll().getOpenInterest() );
+
+            chain.add ( quote );
         }
 
         return chain;
