ee53a51076c7f8723d57f1f967a40fc305e8df60
Philip Foster <phil@the-fosters.net>
2018-05-18 08:35:42 -0400

    Adding follow chain

--- src/com/discernative/etradetools/FollowChain.java
+++ src/com/discernative/etradetools/FollowChain.java
@@ -2,6 +2,7 @@ package com.discernative.etradetools;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,10 +13,21 @@ public class FollowChain {
 
     private static final long SLEEP_TIME_SECONDS = 3;
     private static final long SLEEP_TIME_MILLIS = SLEEP_TIME_SECONDS * 1000;
-    private static final String authTokenFile = "auth_token.dat";
+
+    private static final String DEFAULT_AUTH_TOKEN = "auth_token.dat";
+    private static final String DEFAULT_COMMISSION_PROPERTY = "5.45";
+    private static final String DEFAULT_ASSIGNMENT_FEE_PROPERTY = "4.95";
 
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner ( System.in );
+        String propertiesFile = args[0];
+        
+        Properties props = EtradeTools.getProperties ( propertiesFile );
+        
+        String authTokenFile = props.getProperty ( "auth_token", DEFAULT_AUTH_TOKEN );
+        Double commission = new Double ( props.getProperty ( "commission", DEFAULT_COMMISSION_PROPERTY ) );
+        Double assignmentFee = new Double ( props.getProperty ( "assignment_fee", DEFAULT_ASSIGNMENT_FEE_PROPERTY ) );
+        
         
         // ZION:2018:4:20:CALL:50.000000
         System.out.println( "Enter the option string (Example=ZION:2018:4:20:CALL:50.000000) : " );
@@ -23,15 +35,16 @@ public class FollowChain {
         
         inputScanner.close();
         
-        Pattern regexPattern = Pattern.compile("^([^:]+):(\\d\\d\\d\\d):(\\d\\d?):(\\d\\d?):(CALL|PUT):(\\d+.\\d+)$");
-        Matcher match = regexPattern.matcher(optionString);
-        
+
         String symbol = null;
         Integer year = 0;
         Integer month = 0;
         Integer day = 0;
         String type = null;
         Double strike = 0.0;
+        
+        Pattern regexPattern = Pattern.compile("^([^:]+):(\\d\\d\\d\\d):(\\d\\d?):(\\d\\d?):(CALL|PUT):(\\d+.\\d+)$");
+        Matcher match = regexPattern.matcher(optionString);
         
         if ( match.find() ) {
             symbol = match.group(1);
@@ -53,13 +66,34 @@ public class FollowChain {
         symbolBatch.add ( optionString );
         try {
             while ( true ) {
-                Double stockPrice = 0.0;
-                Double bidPrice = 0.0;
+                Double stockAskPrice = 0.0;
+                Double optionBidPrice = 0.0;
                 
                 for ( QuoteData quoteData : EtradeTools.getQuote ( authToken, symbolBatch ) ) {
-                    System.out.println( "Hey, got a quote" );
+                    String symbolDesc = quoteData.getAll().getSymbolDesc();
+                    System.out.println( "Symbol desc: " + symbolDesc );
+                    Pattern symbolRegexPattern = Pattern.compile("\\$(\\d\\S*) (Call|Put)");
+                    
+                    Matcher symbolMatch = symbolRegexPattern.matcher(symbolDesc);
+                    if ( symbolMatch.find() ) {
+                        optionBidPrice = quoteData.getAll().getBid();
+                    } else {
+                        stockAskPrice = quoteData.getAll().getAsk();
+                    }
                 }
                 
+                System.out.println( "Option bid: " + optionBidPrice );
+                System.out.println( "Stock ask: " + stockAskPrice );
+                System.out.println( "Strike Price: " + strike);
+                
+                Double intrinsicValue = stockAskPrice - strike;
+                Double timeValue = optionBidPrice - intrinsicValue;
+                Double gain = timeValue - ( ( assignmentFee + commission ) / 100 );
+                Double gainPrct = ( 100 * gain ) / stockAskPrice;
+                Double costBasis = ( stockAskPrice - optionBidPrice ) + ( commission / 100 );
+                Double safetyNet = ( 1 - ( costBasis / stockAskPrice ) ) * 100;
+                
+                System.out.println(String.format( "Gain: $%.2f(%.2f%%)", gain*100, gainPrct ));
                 Thread.sleep( SLEEP_TIME_MILLIS );
             }
         } catch (InterruptedException e) {
